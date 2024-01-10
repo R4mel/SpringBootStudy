d3d3a65ccde18f9d8d0d1022b81012ad7f38424c
Ramel <dev_20@naver.com>
2024-01-10 22:01:05 +0900

    springboot

---
+++ springboot-developer/src/main/java/springbootdeveloper/domain/Article.java
@@ -0,0 +1,29 @@
+package springbootdeveloper.domain;
+
+import jakarta.persistence.*;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+
+@Getter
+@Entity // 엔티티로 지정
+@NoArgsConstructor // 파라미터가 없는 기본 생성자 생성
+public class Article {
+    // getter
+    @Id // id 필드를 기본키로 지정
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // 기본키 자동으로 1씩 증가
+    @Column(name = "id", updatable = false)
+    private Long id;
+
+    @Column(name = "title", nullable = false) // "title"이라는 not null 컬럼과 매핑
+    private String title;
+
+    @Column(name = "content", nullable = false)
+    private String content;
+
+    @Builder // 빌더 패턴으로 객체 생성
+    public Article(String title, String content) {
+        this.title = title;
+        this.content = content;
+    }
+}
